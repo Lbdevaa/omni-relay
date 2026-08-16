f932211e11767d468905e88707d72656f55b0201
Lebedev Andrey <lbdev.aa@gmail.com>
2026-08-16 19:29:20 +0500

    add: pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validationSchema } from './config/env.validation';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { IngressModule } from './ingress/ingress.module';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -13,6 +14,15 @@ import { RabbitmqService } from './rabbitmq/rabbitmq.service';
       isGlobal: true,
       validationSchema,
       validationOptions: { abortEarly: false },
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.getOrThrow<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
     }),
     IngressModule,
     RabbitmqModule,
